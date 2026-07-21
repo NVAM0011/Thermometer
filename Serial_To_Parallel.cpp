5f1ebece9cfc46ff9f8dcd110efd9bbdbc7dda06
nicomacdonald <nico.macdonald@gmail.com>
2026-07-21 15:04:59 -0600

    Added Serial_To_Parallel.cpp which will provide functionality for a 74HC595 chip.

---
+++ Serial_To_Parallel.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Nico Macdonald on 2026-07-21.
+//
+
+#include "Serial_To_Parallel.h"
---
+++ Serial_To_Parallel.h
@@ -0,0 +1,14 @@
+//
+// Created by Nico Macdonald on 2026-07-21.
+//
+
+#ifndef THERMOMETER_SERIAL_TO_PARALLEL_H
+#define THERMOMETER_SERIAL_TO_PARALLEL_H
+
+
+class Serial_To_Parallel
+{
+};
+
+
+#endif //THERMOMETER_SERIAL_TO_PARALLEL_H
